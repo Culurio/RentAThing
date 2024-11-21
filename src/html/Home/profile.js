372c105249079d397a7766c7ecbab8afc08a3485
fplacido1 <f.placido@campus.fct.unl.pt>
2024-11-21 20:21:15 +0000

    Adds in profile

--- src/html/Home/profile.js
+++ src/html/Home/profile.js
@@ -8,6 +8,33 @@ function addInfo(){
                     <p id="address">${user.birthDate}</p>`
 }
 
-function addAdds(){
-    window.location.href = "../Home/listItem.html";
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const productGrid = document.querySelector(".items-grid");
+    const template = document.getElementById("items-card-template").content;
+
+    const product_items = JSON.parse(localStorage.getItem('adds')) || [];
+    if (!Array.isArray(product_items)) {
+        console.error("product_items is not an array");
+        return;
+    }
+
+    product_items.forEach((product) => {
+        const clone = template.cloneNode(true);
+        clone.querySelector("img").src = product.image;
+        clone.querySelector("img").alt = product.name;
+        clone.querySelector(".product-name").textContent = product.name;
+        clone.querySelector(".product-price").textContent = `$${product.price}/day`;
+
+        console.log(product.image);
+        const card = clone.querySelector(".product-card");
+        card.dataset.id = product.id;
+        card.dataset.name = product.name;
+        card.dataset.price = product.price;
+        card.dataset.img = product.image;
+        card.dataset.description = product.description;
+        card.dataset.category = product.category;
+
+        productGrid.appendChild(clone);
+    });
+
+});
